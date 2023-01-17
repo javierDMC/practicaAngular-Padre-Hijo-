d7d8eb8593a53812c09c3be9e898e1d6100dd700
javierdelmoralcamacho@gmail.com <javierdelmoralcamacho@gmail.com>
2023-01-17 10:06:32 +0100

    Trabajador importado en servicio y con variables

--- src/app/Servicios/trabajador.service.ts
+++ src/app/Servicios/trabajador.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Trabajador } from '../Modelos/trabajador';
+
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,54 @@ import { Injectable } from '@angular/core';
 export class TrabajadorService {
 
   constructor() { }
+
+  //declaaar la variable titulo
+  titulo="Lista de trabajadores"
+
+  //declarar la variable trabajadores arrat del tipo trabajador
+
+  trabajadores:Array<Trabajador>=[
+    {
+      id:1,
+      nombre: 'Ana',
+      cargo: 'Programadora' ,
+      foto:'1.jpg',
+      votos:0
+    },
+    {
+      id:2,
+      nombre: 'Elena',
+      cargo: 'Administrativa',
+      foto:'2.jpg',
+      votos:0
+    },
+    {
+      id:3,
+      nombre: 'Juan',
+      cargo: 'Analista' ,
+      foto:'3.jpg',
+      votos:0
+    },
+    {
+      id:4,
+      nombre: 'Luis',
+      cargo: 'Programador' ,
+      foto:'4.jpg',
+      votos:0
+    },
+    {
+      id:5,
+      nombre: 'Maria',
+      cargo: 'Diseñadora' ,
+      foto:'5.jpg',
+      votos:0
+    },
+    {
+      id:6,
+      nombre: 'Pedro',
+      cargo: 'Marketing' ,
+      foto:'6.jpg',
+      votos:0
+    }
+ ]
 }
