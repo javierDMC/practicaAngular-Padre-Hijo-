966d0af98e4a400658da330a194477c32283f35d
javierdelmoralcamacho@gmail.com <javierdelmoralcamacho@gmail.com>
2023-01-19 12:53:05 +0100

    Routing en routeLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrabajadoresComponent } from './trabajadores/trabajadores.component';
 import { CardTrabajadorComponent } from './card-trabajador/card-trabajador.component';
+import { HeaderComponent } from './header/header.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TrabajadoresComponent,
-    CardTrabajadorComponent
+    CardTrabajadorComponent,
+    HeaderComponent,
+    InicioComponent,
+    ClientesComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
